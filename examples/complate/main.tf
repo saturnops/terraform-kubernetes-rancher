6709124dbf969fb21a354658e6dbda698808a7e0
Amine Ballalou <ballalouahmedamine@gmail.com>
2023-07-12 22:42:11 +0530

    added resource quota and resource limit

--- examples/complate/main.tf
+++ examples/complate/main.tf
@@ -16,4 +16,22 @@ module "rancher" {
     hostname    = "rancher.prod.in"
     values_yaml = file("./helm/values.yaml")
   }
+  create_resource_limit = true
+  rancher_resource_limit = {
+    pod_max_cpu = "1000m"
+    pod_max_mem = "2048Mi"
+    pod_min_cpu = "500m"
+    pod_min_mem = "1048Mi"
+    pvc_min_storage = "24M"
+    pvc_max_storage = "200G"
+    container_max_cpu = "1000m"
+    container_min_mem = "500Mi"
+    container_min_cpu = "500m"
+    container_max_mem = "1048Mi"
+  }
+  create_resource_quota = true
+  rancher_resource_quota = {
+    no_of_pods = 10
+    no_of_services = 3
+  }
 }
--- variables.tf
+++ variables.tf
@@ -19,3 +19,40 @@ variable "chart_version" {
   default     = "2.7.5-rc5"
   description = "Version of the Rancher chart that will be used to deploy Rancher application."
 }
+
+variable "rancher_resource_limit" {
+  type = any
+  default = {
+    pod_max_cpu = ""
+    pod_max_mem = ""
+    pod_min_cpu = ""
+    pod_min_mem = ""
+    pvc_min_storage = ""
+    pvc_max_storage = ""
+    container_max_cpu = ""
+    container_min_mem = ""
+    container_min_cpu = ""
+    container_max_mem = ""
+  }
+  description = "Specify the configuration settings for Rancher resources limits including cpu, mem, pvc storage for Pods and Container."
+}
+
+variable "create_resource_limit" {
+  description = "value"
+  default = false
+  type = bool
+}
+
+variable "rancher_resource_quota" {
+  type = object({
+    no_of_pods          = number
+    no_of_services      = number
+  })
+  description = "Specify the configuration settings for Rancher resources quota in a namespace."
+}
+
+variable "create_resource_quota" {
+  description = "value"
+  default = false
+  type = bool
+}
