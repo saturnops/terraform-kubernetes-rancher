1691a2de153089f631e8c54b12b516b187af5758
Amine Ballalou <ballalouahmedamine@gmail.com>
2023-07-13 17:01:29 +0530

    added resource quota and resource limit

--- examples/complate/main.tf
+++ examples/complate/main.tf
@@ -19,15 +19,15 @@ module "rancher" {
   create_resource_limit = true
   rancher_resource_limit = {
     pod_max_cpu = "1000m"
-    pod_max_mem = "2048Mi"
-    pod_min_cpu = "500m"
-    pod_min_mem = "1048Mi"
+    pod_max_mem = "4000Mi"
+    pod_min_cpu = "5m"
+    pod_min_mem = "50Mi"
     pvc_min_storage = "24M"
     pvc_max_storage = "200G"
     container_max_cpu = "1000m"
-    container_min_mem = "500Mi"
-    container_min_cpu = "500m"
-    container_max_mem = "1048Mi"
+    container_min_mem = "50Mi"
+    container_min_cpu = "5m"
+    container_max_mem = "3132Mi"
   }
   create_resource_quota = true
   rancher_resource_quota = {
